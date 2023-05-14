52adf291835634cfc4c477266612175cca2fd4d5
ttjoe <ttjoe0@gmail.com>
2023-05-14 18:48:28 +0000

    More works on .env file

--- src/components/Listing/Listing.jsx
+++ src/components/Listing/Listing.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Actions from './Actions'
 import Slider from './Slider'
 const MODE = import.meta.env.MODE
+const SERVER_URL = import.meta.env.SERVER_URL
 
 function Listing() {
     return (
         <div>
-            {MODE}
+            mode: { MODE } - { SERVER_URL}
         </div>
     )
 }
