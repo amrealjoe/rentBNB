0ef0bde685c7e3aa9ce22f764e23ef65689c9eb8
ttjoe <ttjoe0@gmail.com>
2023-05-14 10:17:39 +0000

    [FEATURE] Property Listing Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Listing from './components/Listing/Listing'
 
 function App() {
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>
+        <Listing />
+      </h1>
     </div>
   )
 }
---
+++ src/components/Listing/Listing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Actions from './Actions'
+import Slider from './Slider'
+
+function Listing() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Listing
