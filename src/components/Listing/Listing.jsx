2d68aeb9400a2941f3cc4c4c2096758e53e99d6f
ttjoe <ttjoe0@gmail.com>
2023-05-14 18:34:53 +0000

    [UPDATE] Adding .env file an properties

--- src/components/Listing/Listing.jsx
+++ src/components/Listing/Listing.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Actions from './Actions'
 import Slider from './Slider'
+const MODE = import.meta.env.MODE
 
 function Listing() {
     return (
         <div>
-
+            {MODE}
         </div>
     )
 }
