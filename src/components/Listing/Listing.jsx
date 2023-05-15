ce8e5846dfbc3b826f8246fa401bb398b0195bb7
ttjoe <ttjoe0@gmail.com>
2023-05-15 10:03:42 +0000

    [UPDATE] GET DEV ENV WORKING

--- src/components/Listing/Listing.jsx
+++ src/components/Listing/Listing.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import Actions from './Actions'
 import Slider from './Slider'
 const MODE = import.meta.env.MODE
-const SERVER_URL = import.meta.env.SERVER_URL
+const VITE_ENV = import.meta.env.VITE_ENV
 
 function Listing() {
     return (
         <div>
-            mode: { MODE } - { SERVER_URL}
+            ENV: {MODE} - {VITE_ENV}
         </div>
     )
 }
