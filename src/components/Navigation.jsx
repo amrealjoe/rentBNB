503be64bc632e1b19f807b596e8877738b3c10aa
ttjoe <ttjoe0@gmail.com>
2023-05-15 11:57:32 +0000

    UPDATE: Create navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
 
 function Navigation() {
     return (
-        <div>Navigation</div>
+        
+        <AppBar position="fixed" color="primary">
+          <Toolbar>
+            <Typography variant="h6">
+              logo
+            </Typography>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+            </IconButton>
+          </Toolbar>
+        </AppBar>
     )
 }
 
