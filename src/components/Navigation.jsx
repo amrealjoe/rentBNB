acec2bf4ea1dce694fdfabe3fe8cb2a3491e4a4d
ttjoe <ttjoe0@gmail.com>
2023-05-16 03:48:55 +0000

    UPDATE: insall depend and improve navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,18 +3,27 @@ import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import IconButton from '@mui/material/IconButton'
+import { Avatar } from '@mui/material'
+import styled from "styled-components"
+
+const Bar = styled(Toolbar)`
+    && {
+        justify-content: space-between;
+    }
+`
 
 function Navigation() {
     return (
-        
-        <AppBar position="fixed" color="primary">
-          <Toolbar>
-            <Typography variant="h6">
-              logo
-            </Typography>
-            <IconButton edge="start" color="inherit" aria-label="menu">
-            </IconButton>
-          </Toolbar>
+
+        <AppBar position="static" color="primary" >
+            <Bar>
+                <Typography variant="h6">
+                    logo
+                </Typography>
+                <IconButton edge="start" color="inherit" aria-label="menu">
+                    <Avatar />
+                </IconButton>
+            </Bar>
         </AppBar>
     )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,18 @@
 import { Grid } from '@mui/material'
 import React from 'react'
 import Navigation from '@/Navigation'
+import styled from "styled-components"
+
+const SidePane = styled(Grid)``
+const ContentPane = styled(Grid)``
 
 function Home() {
     return (
         <>
-            <Navigation />
+        <Navigation />
         <Grid container spacing={2}>
-            <Grid item sm={4} md={4}>Side Pane </Grid>
-            <Grid item sm={8} md={8}>Content Pane </Grid>
+            <SidePane item sm={4} md={4}>Side Pane </SidePane>
+            <ContentPane item sm={8} md={8}>Content Pane </ContentPane>
         </Grid>
         </>
     )
