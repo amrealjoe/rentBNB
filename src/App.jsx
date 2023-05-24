91bbe3279a0c912a90cb15e8f412cb7fe06df79d
ttjoe <ttjoe0@gmail.com>
2023-05-24 11:53:25 +0000

    FEATURE: show alert when user is offline

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,52 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { BrowserRouter as Router, Outlet } from "react-router-dom"
 import { ThemeProvider, createTheme } from '@mui/material'
 import Routers from './Routers'
+import MuiSnackbar from '@mui/material/Snackbar';
+import { useOnline } from '@react-hooks-library/core';
+import styled from '@emotion/styled';
+import { Alert } from "@mui/material"
+import { NetworkCell } from '@mui/icons-material';
 
-const theme = createTheme({
-  palette: {
-    // mode: 'dark',
-  }
-})
+const theme = createTheme({})
+
+const Snackbar = styled(MuiSnackbar)`
+`
+
+
 function App() {
+  const isOnline = useOnline()
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => { setOpen(true) }
+  const handleClose = () => { setOpen(false) }
+
+
+  useEffect(() => {
+    if (!isOnline) {
+      handleOpen()
+    } else {
+      handleClose()
+    }
+  }, [isOnline])
+
+
+
 
   return (
     <ThemeProvider theme={theme}>
       <Router>
         <Routers />
       </Router>
+      <Snackbar
+        open={open}
+        autoHideDuration={5000}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+          You're currently offline
+        </Alert>
+      </Snackbar>
     </ThemeProvider>
   )
 }
