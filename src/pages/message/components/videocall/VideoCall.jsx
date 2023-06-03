afd8613c044b39f7a03eb344019bb88c8a93b3f8
ttjoe <ttjoe0@gmail.com>
2023-06-03 12:54:56 +0000

    Added text, increased button size and changed icon color

--- src/pages/message/components/videocall/VideoCall.jsx
+++ src/pages/message/components/videocall/VideoCall.jsx
@@ -1,25 +1,38 @@
 import React from 'react'
-import MuiGrid from '@mui/material/Grid'
 import MuiIconButton from '@mui/material/IconButton'
 import styled from '@emotion/styled'
 import { VideoCallRounded } from '@mui/icons-material'
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
 const Container = styled(Box)`
     width: 100%;
     height: 98vh;
-    border: solid red;
     display: grid;
     place-items: center;
+    align-content: center;
 `
 
 const IconButton = styled(MuiIconButton)`
+    width: 150px;
+    height: 150px;
+    background-color: #ebeaeaa9;
+
+    & > svg {
+        font-size: 5rem;
+        color: #0099FF;
+    }
+    
+`
+
+const Textbox = styled(Typography)`
+    margin-bottom: 20px;
     
 `
 
 function VideoCall() {
     return (
         <Container>
+            <Textbox variant='h5' >Start a video call</Textbox>
             <IconButton>
                 <VideoCallRounded />
             </IconButton>
