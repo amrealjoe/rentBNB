d4b16df823ff1d8a197b65cffc74d8fef4f7b834
ttjoe <ttjoe0@gmail.com>
2023-06-04 12:14:46 +0000

    Add demo content to incoming call component

--- src/pages/message/components/videocall/components/IncomingCall.jsx
+++ src/pages/message/components/videocall/components/IncomingCall.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import { Button, Backdrop, CircularProgress, Paper } from "@mui/material"
+import { Button, Backdrop, Paper, useTheme, Typography, Box} from "@mui/material"
 import styled from '@emotion/styled'
 import MuiIconButton from "@mui/material/IconButton"
 import MuiAvatar from "@mui/material/Avatar"
+import { PhoneAndroidSharp, PhoneIphone } from '@mui/icons-material'
 
 
 const Container = styled(Paper)`
@@ -22,6 +23,11 @@ const IconButton = styled(MuiIconButton)`
     height: 35px;
 `
 
+const FlexBox = styled(Box)`
+    display: flex;
+    flex-direction: column;
+`
+
 function IncomingCall() {
     const [open, setOpen] = useState(false)
     const theme = useTheme()
@@ -33,10 +39,20 @@ function IncomingCall() {
             <Backdrop
                 sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
                 open={open}
-                onClick={handleClose}
+                // onClick={handleClose}
             >
                 <Container>
-                    <CircularProgress color="inherit" />
+                    <Avatar src='/picture.jpg' alt='Jim Jallah' />
+                    <Typography variant='h5' fontWeight={"normal"}>Rose Garteh</Typography>
+                    <Typography variant='h6' fontWeight={"normal"}>Incoming call...</Typography>
+                    <FlexBox>
+                        <IconButton color='blue'>
+                            <PhoneAndroidSharp />
+                        </IconButton>
+                        <IconButton color='primary'>
+                            <PhoneIphone />
+                        </IconButton>
+                    </FlexBox>
                 </Container>
             </Backdrop>
         </>
