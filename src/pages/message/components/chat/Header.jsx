701991b3242380daff991972e8fe3615612c904d
ttjoe <ttjoe0@gmail.com>
2023-06-04 13:55:20 +0000

    Changed chat componet header display name

--- src/pages/message/components/chat/Header.jsx
+++ src/pages/message/components/chat/Header.jsx
@@ -26,7 +26,7 @@ function Header() {
         <Rowbox>
             <Media>
                 <Avatar src='/profile2.jpg' alt='TJ' />
-                <Typography variant='h6' fontWeight={900}>Username</Typography>
+                <Typography variant='h6' fontWeight={900}>Lydia Lydia</Typography>
             </Media>
             
         </Rowbox>
